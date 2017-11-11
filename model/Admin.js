9d5bf184d3a6860db6babd885ecefe70e2a20c34
Mrityunjay Pandey <17mrityunjaypandey@gmail.com>
2017-11-11 23:50:55 +0530

    Admin Database

---
+++ model/Admin.js
@@ -0,0 +1,71 @@
+const mongoose=require("../database/mongo-db");
+const validator = require('validator');
+
+var AdminShema=new mongoose.Schema({
+    Firstname:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    Middlename: {
+        type: String,
+        trim: true,
+    },
+    Lastname:{
+        type:String,
+        trim:true
+    },
+    DateofBirth:{
+        type:Date,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    Sex:{
+        type:String,
+        required:true,
+        maxlength:[1,"Not more than 1"],
+        trim:true
+    },
+    Guardianname:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    EmailId: {
+        type: String,
+        required: [true,'Email ID required'],
+        trim: true,
+        unique: true,
+        minlength: 1,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email address'
+        }
+    },
+    Mobile:{
+        type:String,
+        required: [true,'Mobile Number required'],
+        trim: true,
+        unique: true,
+        minlength: 1,
+        validate: {
+            validator: (value)=>{
+                return validator.isMobilePhone(value,'en-US')
+            },
+            message: '{VALUE} is not a valid Mobile Number'
+        }
+    },
+    Password:{
+        minlength: 6,
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+var Admin=mongoose.model('admin',AdminShema);
+
+module.exports=Admin;
