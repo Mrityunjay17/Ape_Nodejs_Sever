a38b9a5a21fc9c8ef13a47f6cf79eca26496b61e
Mrityunjay Pandey <17mrityunjaypandey@gmail.com>
2017-11-11 23:52:35 +0530

    add create admin request

--- Server/Server.js
+++ Server/Server.js
@@ -1,12 +1,31 @@
 const express=require("express");
+const Admin=require("../model/Admin")
+const bodyParser = require('body-parser');
 
 var app=express();
+var jsonParser = bodyParser.json();
 
+var value;
 
-app.get('/',(req,res)=>{
-    res.send("connection established")
+Admin.count(function (err, count) {
+    if (!err && count === 0) {
+       return value="0";
+    }
+    value="1";
 });
 
+app.get('/',(req,res)=>{
+    res.send(value)
+});
+
+app.post('/create_admin',jsonParser,(req,res)=>{
+    var admin=new Admin(req.body);
+    admin.save().then(()=>{
+        res.send(admin);
+    }).catch((e)=>{
+        res.send(e)
+    })
+});
 
 app.listen(2000,()=>{
     console.log('Server is running in port 2000')
